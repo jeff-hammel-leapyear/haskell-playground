6822126962cc291df4e9659074539e64db8ff9d6
Jeff Hammel <jeff@leapyear.io>
2018-07-23 10:30:51 -0700

    monitor our awesome clock program

--- exe/SecondsSinceEpoch.hs
+++ exe/SecondsSinceEpoch.hs
@@ -1,8 +1,25 @@
+-- | The world's most pointless clock.
+
+{-# LANGUAGE OverloadedStrings #-}
+
 import Control.Concurrent (threadDelay)
+import Data.String (fromString)
 import Data.Time.Clock.POSIX (getPOSIXTime)
+import System.Remote.Gauge (Gauge(..), set)
+import System.Remote.Monitoring (forkServer, getGauge)
+
+
+loop :: Gauge -> IO ()
+loop g = do
+  t <-round <$> getPOSIXTime
+  print t
+  set g t
+  threadDelay 1000000
+  loop g
+
 
 main :: IO ()
 main = do
-  round <$> getPOSIXTime >>= print
-  threadDelay 1000000
-  main
+  handle <- forkServer "localhost" 8000
+  gauge <- getGauge (fromString "SecondsSinceEpoch") handle
+  loop gauge
